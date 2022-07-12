ef7935945d39ec0368c2573460babb0b0ca13894
brain <golenev214@gmail.com>
2022-07-12 15:58:16 +0300

    Initial commit

---
+++ src/main/java/ru/hogwarts/school/service/FacultyService.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.school.service;
+
+import ru.hogwarts.school.model.Faculty;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FacultyService {
+
+    private Map<Long, Faculty> facultyMap = new HashMap<>();
+
+
+}
